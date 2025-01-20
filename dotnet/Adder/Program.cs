d465af2ba0fee20722f46669ef411e601db6b769
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-01-20 22:11:15 +0300

    dotnet: add Adder

---
+++ dotnet/Adder/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter the first number: ");
+int firstNumber = Int32.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int secondNumber = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine(firstNumber + secondNumber);

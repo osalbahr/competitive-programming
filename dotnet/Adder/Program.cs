f1ed8e1aec85ea6447a0ddb01f857ea988ea2eb8
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-02-09 20:04:00 +0300

    dotnet: Fix warning CS8604 possible null argument
    See https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/nullable-warnings?f1url=%3FappId%3Droslyn%26k%3Dk(CS8604)#possible-null-assigned-to-a-nonnullable-reference

--- dotnet/Adder/Program.cs
+++ dotnet/Adder/Program.cs
@@ -1,7 +1,20 @@
-﻿Console.Write("Enter the 1st number: ");
-int firstNumber = Int32.Parse(Console.ReadLine());
+﻿// Parse the first number
+Console.Write("Enter the 1st number: ");
+string? line = Console.ReadLine();
+if (line == null) {
+    Console.Write("Line is null. Please try again.");
+    System.Environment.Exit(1);
+}
+int firstNumber = Int32.Parse(line);
 
+// Parse the second number
 Console.Write("Enter the 2nd number: ");
-int secondNumber = Int32.Parse(Console.ReadLine());
+line = Console.ReadLine();
+if (line == null) {
+    Console.Write("Line is null. Please try again.");
+    System.Environment.Exit(1);
+}
+int secondNumber = Int32.Parse(line);
 
-Console.WriteLine(firstNumber + secondNumber);
+// Output the sum of the two numbers
+Console.WriteLine("Sum: " + (firstNumber + secondNumber));

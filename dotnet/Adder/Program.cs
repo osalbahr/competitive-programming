79b186374358fda88e32d9a64ae00fdeaf9f427f
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-02-09 20:23:17 +0300

    dotnet: use String.IsNullOrEmpty

--- dotnet/Adder/Program.cs
+++ dotnet/Adder/Program.cs
@@ -1,20 +1,20 @@
 ﻿// Parse the first number
 Console.Write("Enter the 1st number: ");
 string? line = Console.ReadLine();
-if (line == null)
+if (String.IsNullOrEmpty(line))
 {
-    Console.Write("Line is null. Please try again.");
-    System.Environment.Exit(1);
+    Console.Write("Line is empty. Please try again.");
+    Environment.Exit(1);
 }
 int firstNumber = Int32.Parse(line);
 
 // Parse the second number
 Console.Write("Enter the 2nd number: ");
 line = Console.ReadLine();
-if (line == null)
+if (String.IsNullOrEmpty(line))
 {
-    Console.Write("Line is null. Please try again.");
-    System.Environment.Exit(1);
+    Console.Write("Line is empty. Please try again.");
+    Environment.Exit(1);
 }
 int secondNumber = Int32.Parse(line);
 

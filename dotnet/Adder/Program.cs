5ed00c30041ae23a9553cdfdaf2b2e457672b1c4
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-02-09 19:53:48 +0300

    dotnet: prompt alignment

--- dotnet/Adder/Program.cs
+++ dotnet/Adder/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.Write("Enter the first number: ");
+﻿Console.Write("Enter the 1st number: ");
 int firstNumber = Int32.Parse(Console.ReadLine());
 
-Console.Write("Enter the second number: ");
+Console.Write("Enter the 2nd number: ");
 int secondNumber = Int32.Parse(Console.ReadLine());
 
 Console.WriteLine(firstNumber + secondNumber);

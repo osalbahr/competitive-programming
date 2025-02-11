9b32ff44ba58baf35f559d8abca54970dabf8f07
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-02-11 18:57:52 +0300

    Adder: use TryParse

--- dotnet/Adder/Program.cs
+++ dotnet/Adder/Program.cs
@@ -3,20 +3,34 @@ Console.Write("Enter the 1st number: ");
 var line = Console.ReadLine();
 if (String.IsNullOrEmpty(line))
 {
-    Console.Write("Line is empty. Please try again.");
+    Console.WriteLine("Line is empty. Please try again.");
     Environment.Exit(1);
 }
-var firstNumber = Int32.Parse(line);
+
+int firstNumber;
+var success = int.TryParse(line, out firstNumber);
+if (!success)
+{
+    Console.WriteLine("Invalid number. Please try again.");
+    Environment.Exit(1);
+}
 
 // Parse the second number
 Console.Write("Enter the 2nd number: ");
 line = Console.ReadLine();
 if (String.IsNullOrEmpty(line))
 {
-    Console.Write("Line is empty. Please try again.");
+    Console.WriteLine("Line is empty. Please try again.");
     Environment.Exit(1);
 }
-var secondNumber = Int32.Parse(line);
+
+int secondNumber;
+success = int.TryParse(line, out secondNumber);
+if (!success)
+{
+    Console.WriteLine("Invalid number. Please try again.");
+    Environment.Exit(1);
+}
 
 // Output the sum of the two numbers
 Console.WriteLine("Sum: " + (firstNumber + secondNumber));

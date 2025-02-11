07c2b01fc4ab4ef576178d6ef8c5248c7ef48526
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-02-11 18:46:28 +0300

    Adder: use `var`

--- dotnet/Adder/Program.cs
+++ dotnet/Adder/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Parse the first number
 Console.Write("Enter the 1st number: ");
-string? line = Console.ReadLine();
+var line = Console.ReadLine();
 if (String.IsNullOrEmpty(line))
 {
     Console.Write("Line is empty. Please try again.");
     Environment.Exit(1);
 }
-int firstNumber = Int32.Parse(line);
+var firstNumber = Int32.Parse(line);
 
 // Parse the second number
 Console.Write("Enter the 2nd number: ");
@@ -16,7 +16,7 @@ if (String.IsNullOrEmpty(line))
     Console.Write("Line is empty. Please try again.");
     Environment.Exit(1);
 }
-int secondNumber = Int32.Parse(line);
+var secondNumber = Int32.Parse(line);
 
 // Output the sum of the two numbers
 Console.WriteLine("Sum: " + (firstNumber + secondNumber));

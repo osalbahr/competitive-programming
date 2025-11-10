edafe16d329ca33428f505ca0f30ca26b442d406
Osama Albahrani <osalbahr@gmail.com>
2025-11-10 12:03:20 +0300

    atcoder: Add abc319A

---
+++ atcoder/abc319/a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+name_to_rating = {
+    "tourist": 3858,
+    "ksun48": 3679,
+    "Benq": 3658,
+    "Um_nik": 3648,
+    "apiad": 3638,
+    "Stonefeang": 3630,
+    "ecnerwala": 3613,
+    "mnbvmar": 3555,
+    "newbiedmy": 3516,
+    "semiexp": 3481,
+}
+
+s = input()
+
+print(name_to_rating[s])

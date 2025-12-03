4d17810e68ecf374ceeeb649c063a816ebaa302d
Osama Albahrani <osalbahr@gmail.com>
2025-12-03 09:34:57 +0300

    aoc: Optimize by early break

--- adventofcode/2025/day03.py
+++ adventofcode/2025/day03.py
@@ -4,16 +4,21 @@ import sys
 
 total = 0
 for joltages in sys.stdin:
+    joltages = joltages.strip()
+
     current_max = -1
     for i in range(9, -1, -1):
-        if (idx1 := joltages.find(str(i))) == -1:
+        if (idx1 := joltages.find(str(i))) == -1 or idx1 > len(joltages) - 2:
             continue
 
         for j in range(9, -1, -1):
-            if (idx2 := joltages.find(str(j), idx1 + 1)) == -1:
+            if (idx2 := joltages.find(str(j), idx1 + 1)) == -1 or idx2 > len(joltages) - 2 + 1:
                 continue
 
             current_max = max(current_max, int(joltages[idx1] + joltages[idx2]))
+            break
+        
+        break
 
     total += current_max
 

39edacc8f4a582b1fd12b481f707e769adfc47cb
Osama Albahrani <osalbahr@gmail.com>
2025-12-03 09:22:14 +0300

    aoc: Optimize by counting backwards

--- adventofcode/2025/day03.py
+++ adventofcode/2025/day03.py
@@ -5,11 +5,11 @@ import sys
 total = 0
 for joltages in sys.stdin:
     current_max = -1
-    for i in range(1, 9 + 1):
+    for i in range(9, -1, -1):
         if (idx1 := joltages.find(str(i))) == -1:
             continue
 
-        for j in range(1, 9 + 1):
+        for j in range(9, -1, -1):
             if (idx2 := joltages.find(str(j), idx1 + 1)) == -1:
                 continue
 

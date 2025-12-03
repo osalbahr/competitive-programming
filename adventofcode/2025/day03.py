19298f172c532e4ebdfe1e84177aede55c72fb52
Osama Albahrani <osalbahr@gmail.com>
2025-12-03 08:40:06 +0300

    aoc: Optimize day 3 part 1

--- adventofcode/2025/day03.py
+++ adventofcode/2025/day03.py
@@ -5,9 +5,15 @@ import sys
 total = 0
 for joltages in sys.stdin:
     current_max = -1
-    for i in range(len(joltages) - 1):
-        for j in range(i + 1, len(joltages)):
-            current_max = max(current_max, int(joltages[i] + joltages[j]))
+    for i in range(1, 9 + 1):
+        if (idx1 := joltages.find(str(i))) == -1:
+            continue
+
+        for j in range(1, 9 + 1):
+            if (idx2 := joltages.find(str(j), idx1 + 1)) == -1:
+                continue
+
+            current_max = max(current_max, int(joltages[idx1] + joltages[idx2]))
 
     total += current_max
 

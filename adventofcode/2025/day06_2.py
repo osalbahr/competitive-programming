8fd10ab2129d3540257a90f9dd9d69e88dbb4258
Osama Albahrani <osalbahr@gmail.com>
2025-12-06 13:21:03 +0300

    aoc: Add day 6 part 2

--- adventofcode/2025/day06_2.py
+++ adventofcode/2025/day06_2.py
@@ -6,10 +6,12 @@ rows = [line.strip("\n") for line in sys.stdin]
 num_rows = rows[:-1]
 operation_row = rows[len(rows) - 1]
 
+symbols = []
 positions = []
 
 for i in range(len(operation_row)):
     if operation_row[i] in ["+", "*"]:
+        symbols.append(operation_row[i])
         positions.append(i)
 
 col_tuples = []
@@ -22,4 +24,35 @@ num_groups = []
 for start, end in col_tuples:
     num_groups.append([row[start : end + 1] for row in num_rows])
 
-print(num_groups)
+
+def transform(num_group):
+    transformed_group = []
+    for i in range(len(num_group[0])):
+        current_num = ""
+        for row in num_group:
+            digit = row[i]
+            if digit != " ":
+                current_num += digit
+
+        transformed_group.append(current_num)
+
+    return transformed_group
+
+
+total = 0
+for i in range(len(symbols)):
+    transformed_nums = transform(num_groups[i])[:-1]
+    print(transform(num_groups[i]))
+
+    if symbols[i] == "+":
+        current_total = 0
+        for num in transformed_nums:
+            current_total += int(num)
+    else:
+        current_total = 1
+        for num in transformed_nums:
+            current_total *= int(num)
+
+    total += current_total
+
+print(total)

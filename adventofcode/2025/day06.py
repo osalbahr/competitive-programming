13edd4764bc731d18993e58830eb612bbd0311c8
Osama Albahrani <osalbahr@gmail.com>
2025-12-06 11:09:57 +0300

    aoc: Simplify day 6 part 1

--- adventofcode/2025/day06.py
+++ adventofcode/2025/day06.py
@@ -8,16 +8,19 @@ row_count = len(lines)
 col_count = len(lines[0])
 
 total = 0
+
 for col_idx in range(col_count):
-    operation = lines[row_count - 1][col_idx]
+    col = [line[col_idx] for line in lines]
+
+    operation = col[len(col) - 1]
 
     if operation == "+":
         current_total = 0
-        for row_idx in range(len(lines) - 1):
+        for row_idx in range(len(col) - 1):
             current_total += int(lines[row_idx][col_idx])
     else:
         current_total = 1
-        for row_idx in range(len(lines) - 1):
+        for row_idx in range(len(col) - 1):
             current_total *= int(lines[row_idx][col_idx])
 
     total += current_total

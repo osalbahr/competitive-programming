583a34674cbbbe67a6ef2784b20b2f1ed2b5dc18
Osama Albahrani <osalbahr@gmail.com>
2025-12-11 16:33:05 +0300

    Add day 11 part 2 (too slow)

--- adventofcode/2025/day11_2.py
+++ adventofcode/2025/day11_2.py
@@ -10,23 +10,22 @@ for line in sys.stdin:
     next_list = tuple(next_list.split())
     edge_list[start] = next_list
 
-node_to_paths = {"out": [("out",)]}
-
-def get_paths(node):
-    if node in node_to_paths:
-        return node_to_paths[node]
-
-    paths = []
-    for next_node in edge_list[node]:
-        for path in get_paths(next_node):
-            paths.append((node,) + path)
-
-    node_to_paths[node] = paths
-    return node_to_paths[node]
-
 total = 0
-for path in get_paths("svr"):
-    if "dac" in path and "fft" in path:
-        total += 1
+
+def count_paths(path):
+    node = path[-1]
+
+    if node == "out":
+        if "dac" in path and "fft" in path:
+            global total
+            total += 1
+
+        return
+
+    for next_node in edge_list[node]:
+        if next_node not in path:
+            count_paths(path + (next_node,))
+
+count_paths(("svr",))
 
 print(total)

d5e5c0a00ac4b7f030f245b01b737cffee32d892
Osama Albahrani <osalbahr@gmail.com>
2026-02-20 19:23:58 +0300

    aoc: 2015 day 7 stuck

--- adventofcode/2015/day07.py
+++ adventofcode/2015/day07.py
@@ -2,29 +2,46 @@
 
 import sys
 
-table = {}
-
 def convert(token):
     try:
         return int(token)
     except ValueError:
         return table[token]
 
+table = {}
+is_evaluated = set()
+
+lines = []
+
 for line in sys.stdin:
-    tokens = line.strip().split()
+    lines.append(line.strip())
 
-    if len(tokens) == 3:
-        table[tokens[-1]] = convert(tokens[0])
-    elif len(tokens) == 5:
-        if tokens[1] == "AND":
-            table[tokens[-1]] = convert(tokens[0]) & convert(tokens[2])
-        elif tokens[1] == "OR":
-            table[tokens[-1]] = convert(tokens[0]) | convert(tokens[2])
-        elif tokens[1] == "LSHIFT":
-            table[tokens[-1]] = convert(tokens[0]) << convert(tokens[2])
-        elif tokens[1] == "LSHIFT":
-            table[tokens[-1]] = convert(tokens[0]) >> convert(tokens[2])
-    elif len(tokens) == 4:
-        table[tokens[-1]] = (~ convert(tokens[1])) & 0xFFFF
+while len(lines) > len(is_evaluated):
+    for i, line in enumerate(lines):
+        if i in is_evaluated:
+            continue
 
-print(table["a"])
+        tokens = line.split()
+
+        try:
+            if len(tokens) == 3:
+                table[tokens[-1]] = convert(tokens[0])
+            elif len(tokens) == 5:
+                if tokens[1] == "AND":
+                    table[tokens[-1]] = convert(tokens[0]) & convert(tokens[2])
+                elif tokens[1] == "OR":
+                    table[tokens[-1]] = convert(tokens[0]) | convert(tokens[2])
+                elif tokens[1] == "LSHIFT":
+                    table[tokens[-1]] = convert(tokens[0]) << convert(tokens[2])
+                elif tokens[1] == "LSHIFT":
+                    table[tokens[-1]] = convert(tokens[0]) >> convert(tokens[2])
+            elif len(tokens) == 4:
+                table[tokens[-1]] = (~ convert(tokens[1])) & 0xFFFF  
+
+            is_evaluated.add(i)
+            print(len(is_evaluated))
+            break
+        except KeyError:
+            continue
+
+print(table)

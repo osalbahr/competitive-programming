546ac44c54c36d072596e59d7793344523eb63ec
Osama Albahrani <osalbahr@gmail.com>
2026-03-28 17:52:44 +0300

    aoc: Condense template.py

--- codeforces/template.py
+++ codeforces/template.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 
 
-def solve():
-    ...
+def solve(): ...
 
 
-def main():
-    t = int(input())
-    for _ in range(t):
-        solve()
-
-
-if __name__ == "__main__":
-    main()
+for _ in range(int(input())):
+    solve()
